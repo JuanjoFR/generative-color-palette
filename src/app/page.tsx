efe13231a3826b382d3869be25243b3829b9833e
Juanjo Fernández <juanjo.fernandez@haa.st>
2025-06-03 10:51:11 +0200

    Install shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-blue-100 via-pink-100 to-purple-200">
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </main>
+  );
 }
